ed5700302f490a161e01ff76cb3ef1df9ecf065b
Abhishek Kushwaha <abhikushwaha335@gmail.com>
2026-01-23 14:43:29 +0530

    update backend

--- backend/main.py
+++ backend/main.py
@@ -65,6 +65,6 @@ import uvicorn
 def root():
     return {"status": "Backend running"}
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8000))
+#     uvicorn.run(app, host="0.0.0.0", port=port)
